891f107bd4484aa87fbc60874ce75ecd681b54f0
Lourenco <lo.passos93@gmail.com>
2021-05-04 14:32:01 -0300

    chore: project setup with CRA

---
+++ web/src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css';
+import IssuesList from './components/IssuesList';
+
+function App() {
+  return <IssuesList />;
+}
+
+export default App;
---
+++ web/src/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client/react';
+import { ApolloProvider as ApolloHooksProvider } from '@apollo/react-hooks';
+import { setContext } from '@apollo/client/link/context';
+
+import App from './App';
+
+const httpLink = createHttpLink({
+  uri: 'https://api.github.com/graphql',
+});
+
+const authLink = setContext((_, { headers }) => ({
+  headers: {
+    authorization: 'Bearer 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',
+    'content-Type': 'application/json',
+  },
+}));
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <ApolloHooksProvider client={client}>
+      <App />
+    </ApolloHooksProvider>
+  </ApolloProvider>,
+  document.getElementById('root'),
+);
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals

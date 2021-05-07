b5892c7444868a48eb008c992d08a6e0c99e3341
Lourenco <lo.passos93@gmail.com>
2021-05-07 00:24:02 -0300

    feature: add header component

---
+++ web/src/components/Header/index.tsx
@@ -0,0 +1,32 @@
+import React, { ChangeEventHandler } from 'react';
+import { IssueState } from '../../generated/graphql';
+import logo from '../../logo.svg';
+
+interface Props {
+  filterValue: IssueState;
+  onChange: ChangeEventHandler<HTMLSelectElement>;
+}
+
+const Header: React.FC<Props> = (props) => {
+  const { onChange, filterValue } = props;
+
+  return (
+    <header className="header">
+      <div className="header-items">
+        <div className="header-items__headline">
+          <img alt="React Logo" src={logo} />
+          <h1>React.js Issues</h1>
+        </div>
+      </div>
+      <div className="header-items__filter">
+        <select onChange={onChange}>
+          <option value={-1}>ALL</option>
+          <option value="OPEN"> OPEN </option>
+          <option value="CLOSED"> CLOSED </option>
+        </select>
+      </div>
+    </header>
+  );
+};
+
+export default Header;

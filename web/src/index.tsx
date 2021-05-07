4eff226b02ac563cb5062fbaffefb480dc8b0fbb
Lourenco <lo.passos93@gmail.com>
2021-05-07 16:02:29 -0300

    chore: use const for token

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -13,9 +13,11 @@ const httpLink = createHttpLink({
   uri: 'https://api.github.com/graphql',
 });
 
+const token = process.env.REACT_APP_GITHUB_TOKEN || 'CHECK_YOUR_TOKEN';
+
 const authLink = setContext((_, { headers }) => ({
   headers: {
-    authorization: `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
+    authorization: `Bearer ${token}`,
     'content-Type': 'application/json',
   },
 }));
@@ -36,5 +38,3 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
-
